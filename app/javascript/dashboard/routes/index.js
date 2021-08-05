2008de7679016464e07449d560e0d4265eac055c
galliaCodeWorld <hperfomancedev@gmail.com>
2021-08-06 00:15:50 +0900

    fr-1

--- app/javascript/dashboard/routes/index.js
+++ app/javascript/dashboard/routes/index.js
@@ -110,16 +110,15 @@ const validateRouteAccess = (to, from, next) => {
 
 // protecting routes
 router.beforeEach((to, from, next) => {
-  next()
-  // if (!to.name) {
-  //   const user = auth.getCurrentUser();
-  //   if (user) {
-  //     return next(frontendURL(`accounts/${user.account_id}/dashboard`));
-  //   }
-  //   return next('/app/login');
-  // }
+  if (!to.name) {
+    const user = auth.getCurrentUser();
+    if (user) {
+      return next(frontendURL(`accounts/${user.account_id}/dashboard`));
+    }
+    return next('/app/login');
+  }
 
-  // return validateRouteAccess(to, from, next);
+  return validateRouteAccess(to, from, next);
 });
 
 export default router;
