6a6fb687ca05892603e5437cf219b5fb35cb298b
James <jamesdcoulter1972@gmail.com>
2021-08-12 18:43:45 +0900

    leads-1.1.0

--- app/javascript/dashboard/routes/dashboard/leads/index.vue
+++ app/javascript/dashboard/routes/dashboard/leads/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="leads-page row">
-    <div class="left-wrap" :class="wrapClas">
+    <div class="left-wrap" :class="wrapClass">
       <LeadsHeader
         :query="leadState.query"
         :queryHistory="leadState.queryHistory"
@@ -19,18 +19,20 @@
   import LeadsTable from './components/LeadTable.vue';
   export default {
     name: 'entity-leads',
-    component: {
+    components: {
       LeadsHeader,
       LeadsTable,
     },
     computed: {
       ...mapGetters({
         leadState: 'enLeads/getState'
-      })
+      }),
+      wrapClass() {
+        return this.leadState.editID === -1 ? 'medium-12' : 'medium-9';
+      }
     },
     beforeRouteEnter(to, from, next) {
-      store.dispatch('adLeads/search')
-      .then(() => {
+      store.dispatch('enLeads/search').then(() => {
         console.log('test api...', store)
         next()
       })
@@ -38,7 +40,6 @@
   };
 </script>
 <style lang="scss" scoped>
-@import "~vue-material/theme/engine";
 @import url("https://fonts.googleapis.com/css?family=Material+Icons");
 .leads-page {
   width: 100%;
--- app/javascript/dashboard/routes/dashboard/settings/groups/Index.vue
+++ app/javascript/dashboard/routes/dashboard/settings/groups/Index.vue
@@ -1,51 +1,268 @@
 <template>
-  <div class="leads-page row">
-    <div class="left-wrap" :class="wrapClas">
-      <LeadsHeader
-        :query="leadState.query"
-        :queryHistory="leadState.queryHistory"
-      />
-      <LeadsTable
-        :leads="leadState.leads"
-      />
+  <div class="column content-box">
+    <woot-button
+      color-scheme="success"
+      class-names="button--fixed-right-top"
+      icon="ion-android-add-circle"
+      @click="openAddPopup()"
+    >
+      {{ $t('GROUP_MGMT.HEADER_BTN_TXT') }}
+    </woot-button>
+
+    <!-- List Agents -->
+    <div class="row">
+      <div class="small-12 columns">
+        <div>
+          <p v-if="!groupState.groups.length">
+            {{ $t('GROUP_MGMT.LIST.404') }}
+          </p>
+          <table v-else class="woot-table">
+            <tbody>
+              <tr v-for="(group, index) in groupState.groups" :key="'ts-'+index">
+                <!-- Agent Name + Email -->
+                <td>
+                  <span class="group-name">{{ group.name }}</span>
+                </td>
+                <td>
+                  <span class="group-">{{ group.name }}</span>
+                </td>
+                <!-- Actions -->
+                <td>
+                  <div class="button-wrapper">
+                    <woot-button
+                      v-if="showEditAction(group)"
+                      variant="link"
+                      color-scheme="secondary"
+                      icon="ion-edit"
+                      class-names="grey-btn"
+                      @click="openEditPopup(group)"
+                    >
+                      {{ $t('GROUP_MGMT.EDIT.BUTTON_TEXT') }}
+                    </woot-button>
+                    <woot-button
+                      v-if="showDeleteAction(group)"
+                      variant="link"
+                      color-scheme="secondary"
+                      icon="ion-close-circled"
+                      class-names="grey-btn"
+                      @click="openDeletePopup(group, index)"
+                    >
+                      {{ $t('GROUP_MGMT.DELETE.BUTTON_TEXT') }}
+                    </woot-button>
+                  </div>
+                </td>
+              </tr>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
+    <!-- Add Agent -->
+   <!--  <woot-modal :show.sync="showAddPopup" :on-close="hideAddPopup">
+      <add-group :on-close="hideAddPopup" />
+    </woot-modal> -->
+    <!-- Edit Agent -->
+    <!-- <woot-modal :show.sync="showEditPopup" :on-close="hideEditPopup">
+      <edit-group
+        v-if="showEditPopup"
+        :id="currentAgent.id"
+        :name="currentAgent.name"
+        :on-close="hideEditPopup"
+      />
+    </woot-modal> -->
+    <!-- Delete Agent -->
+    <woot-delete-modal
+      :show.sync="showDeletePopup"
+      :on-close="closeDeletePopup"
+      :on-confirm="confirmDeletion"
+      :title="$t('GROUP_MGMT.DELETE.CONFIRM.TITLE')"
+      :message="deleteMessage"
+      :confirm-text="deleteConfirmText"
+      :reject-text="deleteRejectText"
+    />
   </div>
 </template>
 
-<script>
-  import { mapGetters } from "vuex";
-  import store from '../../../store';
-  import LeadsHeader from './components/LeadHeader.vue';
-  import LeadsTable from './components/LeadTable.vue';
-  export default {
-    name: 'entity-leads',
-    component: {
-      LeadsHeader,
-      LeadsTable,
+<!-- script>
+import { mapGetters } from 'vuex';
+import store from '../../../../store'
+import Edit from './edit.vue';
+import Detail from './detail.vue';
+
+export default {
+  name: 'ad-groups',
+  components: {
+    Edit,
+    Detail,
+  },
+  props: {
+    contactId: {
+      type: [String, Number],
+      default: 0,
     },
-    computed: {
-      ...mapGetters({
-        leadState: 'enLeads/getState'
-      })
-    },
-    beforeRouteEnter(to, from, next) {
-      store.dispatch('adLeads/search')
-      .then(() => {
-        console.log('test api...', store)
-        next()
-      })
-    },
-  };
+  },
+  computed: {
+    ...mapGetters({
+      groupState: 'adGroups/getState',
+      userState: 'adUsers/getState',
+    }),
+  },
+  beforeRouteEnter(to, from, next) {
+    Promise.all([
+      store.dispatch('adUsers/search'),
+      store.dispatch('adGroups/get'),
+    ]).then(() => {
+      console.log(store)
+      next();
+    });
+  },
+  methods: {
+    showModal() {
+      this.$store.dispatch('adGroups/editID', null)
+    }
+  },
+};
 </script>
-<style lang="scss" scoped>
-@import "~vue-material/theme/engine";
-@import url("https://fonts.googleapis.com/css?family=Material+Icons");
-.leads-page {
-  width: 100%;
+
+<style lang="scss">
+.ad-groups {
+  .md-card .md-card-header {
+    padding: 0 12px;
+  }
+  p {
+    margin-bottom: 0;
+  }
 }
-.left-wrap {
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-}
-</style>
+</style> -->
+<script>
+  import { mapGetters } from 'vuex';
+import globalConfigMixin from 'shared/mixins/globalConfigMixin';
+// import AddTag from './AddTag.vue';
+// import EditTag from './EditTag';
+import store from '../../../../store'
+
+export default {
+  components: {
+    // AddTag,
+    // EditTag,
+  },
+  mixins: [globalConfigMixin],
+  data() {
+    return {
+      loading: {},
+      showAddPopup: false,
+      showDeletePopup: false,
+      showEditPopup: false,
+      tagAPI: {
+        message: '',
+      },
+      currentAgent: {},
+    };
+  },
+  beforeRouteEnter(to, from, next) {
+    store.dispatch('adTags/get').then(() => {
+      next();
+    });
+  },
+  computed: {
+    ...mapGetters({
+      tagState: 'adTags/getState',
+      globalConfig: 'globalConfig/get',
+    }),
+    deleteConfirmText() {
+      return `${this.$t('TAG_MGMT.DELETE.CONFIRM.YES')} ${
+        this.currentAgent.name
+      }`;
+    },
+    deleteRejectText() {
+      return `${this.$t('TAG_MGMT.DELETE.CONFIRM.NO')} ${
+        this.currentAgent.name
+      }`;
+    },
+    deleteMessage() {
+      return `${this.$t('TAG_MGMT.DELETE.CONFIRM.MESSAGE')} ${
+        this.currentAgent.name
+      } ?`;
+    },
+  },
+  mounted() {
+    this.$store.dispatch('tags/get');
+  },
+  methods: {
+    showEditAction(tag) {
+      return this.currentUserId !== tag.id;
+    },
+    showDeleteAction(tag) {
+      if (this.currentUserId === tag.id) {
+        return false;
+      }
+
+      if (!tag.confirmed) {
+        return true;
+      }
+
+      if (tag.role === 'administrator') {
+        return this.verifiedAdministrators().length !== 1;
+      }
+      return true;
+    },
+    verifiedAdministrators() {
+      return this.tagList.filter(
+        tag => tag.role === 'administrator' && tag.confirmed
+      );
+    },
+    // Edit Function
+    openAddPopup() {
+      this.showAddPopup = true;
+    },
+    hideAddPopup() {
+      this.showAddPopup = false;
+    },
+
+    // Edit Function
+    openEditPopup(tag) {
+      this.showEditPopup = true;
+      this.currentAgent = tag;
+    },
+    hideEditPopup() {
+      this.showEditPopup = false;
+    },
+
+    // Delete Function
+    openDeletePopup(tag) {
+      this.showDeletePopup = true;
+      this.currentAgent = tag;
+    },
+    closeDeletePopup() {
+      this.showDeletePopup = false;
+    },
+    confirmDeletion() {
+      this.loading[this.currentAgent.id] = true;
+      this.closeDeletePopup();
+      this.deleteAgent(this.currentAgent.id);
+    },
+    async deleteAgent(id) {
+      try {
+        this.$store.dispatch('adTags/delete', id).then(() => {
+          this.$store.dispatch('adTags/get').then(() => {
+            this.$store.dispatch('global/setMsg', `Deleted "${tmp}" tag..`)
+          })
+        })
+        this.showAlert(this.$t('TAG_MGMT.DELETE.API.SUCCESS_MESSAGE'));
+      } catch (error) {
+        this.showAlert(this.$t('TAG_MGMT.DELETE.API.ERROR_MESSAGE'));
+      }
+    },
+    // Show SnackBar
+    showAlert(message) {
+      // Reset loading, current selected tag
+      this.loading[this.currentAgent.id] = false;
+      this.currentAgent = {};
+      // Show message
+      this.tagAPI.message = message;
+      bus.$emit('newToastMessage', message);
+    },
+  },
+};
+</script>
